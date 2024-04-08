edf63d0279af0dbdb2f968a4e6f848bdc0d3052b
MauroQui <pmq244@gmail.com>
2024-04-08 17:36:11 -0500

    Creando proyecto en React

---
+++ src/assets/componentes/Header.jsx
@@ -0,0 +1,22 @@
+
+const Header = () => {
+    return <div>
+        <img
+          src="imagenes/letter-c-trolley.jpg"
+          class="logo"
+          alt="Logo de la compañia"
+        />
+        <nav>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Carrito de Compras</a></li>
+          </ul>
+        </nav>
+        <a href="" class="btn">Login</a>
+                      
+    </div>
+    
+} 
+
+export default Header
---
+++ src/assets/componentes/Layout.jsx
@@ -0,0 +1,20 @@
+const Layout = () => {
+    return <div>
+        <img
+          src=""
+          class="logo"
+          alt="Logo de la compañia"
+        />
+        <nav>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Carrito de Compras</a></li>
+          </ul>
+        </nav>
+        <a href="" class="btn">Login</a>
+       
+    </div>
+}
+
+export default Layout
